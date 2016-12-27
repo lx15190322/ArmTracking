63e62be3cd36c30b4b4d245a072e4387f44b9d50
Juan <juan@juan-VirtualBox.(none)>
2016-12-27 02:51:53 -0800

    newest version

--- scripts/talker.py
+++ scripts/talker.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import UInt16
+#from std_msgs.msg import Float32
 # Import OpenCV
 import cv2
 # Import Numpy
 import numpy as np
+from evdev import InputDevice
+from select import select
+
 #ballsize 50.8mm
 #camera
 ##pixel 3um*3um = 0.003 * 0.003 mm^2
@@ -17,8 +21,31 @@ lens = 3.5
 ballsize = 50.8
 lock = 0
 #command = 50
+logfile = open('output.txt','w')
 lost_count = 0
 pub = rospy.Publisher('servo', UInt16, queue_size=10)
+pub4 = rospy.Publisher('servo4', UInt16, queue_size=10)
+pub_dis = rospy.Publisher('dis', UInt16, queue_size=10)
+pub_dir = rospy.Publisher('dir', UInt16, queue_size=10)
+
+dis_set = []
+dir_hist = []
+
+def detectInputKey():
+    dev = InputDevice('/dev/input/event2')
+    flag = True
+    while flag:
+        select([dev], [] ,[])
+        for event in dev.read():
+	    if event.value == 30:
+		pub4.publish(0)
+                #print 'detect keyboard event (a)'
+            elif event.value == 32:
+		pub4.publish(55)
+                #print 'detect keyboard event (d)'
+            elif event.value == 14:
+		flag = False
+                #print 'detect exit event'
 
 def callback(data):
     global lock
@@ -28,16 +55,43 @@ def talker(angle):
     #global command
     global pub
     global lock
-    
+    global dir_hist
     #rate = rospy.Rate(0.5) # 10hz
-    
-    '''if command == 50:
-	command = 80
-    elif command == 80:
-	command = 50
-    '''	
+    if len(dir_hist) > 10:
+	dir_hist.remove(dir_hist[0])
+
+    judge = 1
     if angle < 0:
+	if angle < -judge:
+	    dir_hist.append(1)
+	else:
+	    dir_hist.append(0)
 	angle = int(np.abs(angle)) + 180
+    elif angle >=0 and angle < 180:
+	if angle > judge:
+	    dir_hist.append(-1)
+	else:
+	    dir_hist.append(0)
+    else:
+	dir_hist.append(0)
+
+    '''if angle < -2:
+	pub_dir.publish(1)
+    elif angle > 2:
+	pub_dir.publish(3)
+    else:
+	pub_dir.publish(2)'''
+
+    if len(dir_hist) > 10:
+	if sum(dir_hist)>5:
+	    pub_dir.publish(1)
+	elif sum(dir_hist)<-5:
+	    pub_dir.publish(3)
+	else:
+	    pub_dir.publish(2)
+    else:
+	pub_dir.publish(2)
+
     if lock == 0:
     	pub.publish(angle)
     #rate.sleep()
@@ -88,19 +142,26 @@ if __name__ == '__main__':
                 ballcenter = [(x+w/2)*1.0, (y+h/2)*1.0]
 	        cv2.circle(frame, (x+w/2, y+h/2), (w+h)/4, (0, 0, 255), 3)
                 dis = lens*ballsize/(max([w,h])*pixel)
-	    
+		pub_dis.publish(int(dis))
+		dis_set.append(dis)
 	        #dev = np.sqrt((ballcenter[0]-center[0])*(ballcenter[0]-center[0]) + (ballcenter[1]-center[1])*(ballcenter[1]-center[1]))
 	        dev = ballcenter[0] - center[0]	    
 	        realdev = dev*ballsize/(max([w,h]))
 	        #des_angle = np.floor(np.arcsin(realdev/dis)*180.0/np.pi)
-	        des_angle = np.floor(np.arctan(dev*pixel/lens)*180.0/np.pi)
+	        des_angle = np.floor((200+dis/10)/dis * np.arctan(dev*pixel/lens)*180.0/np.pi)
                 cv2.putText(frame,str(dis),(x+w/2, y+h/2),cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 1)
 	        try:
         	    #talker()
 		    a_input = int(des_angle)
+		    if dis < 400:
+		    	a_input = int(a_input/4)
+		    elif dis >= 400 and dis <900:
+			a_input = int(a_input/2)
 	            talker(a_input)
     	        except rospy.ROSInterruptException:
         	    pass
+		if dis < 200:
+		    break
 	    else:
 		lost_count += 1
 		if lost_count > 10:
@@ -121,6 +182,15 @@ if __name__ == '__main__':
         # If escape is pressed close all windows
         if k == 27:
             break
+    detectInputKey()
 
+    write_str = "Start writing record %s" % rospy.get_time()
+    rospy.loginfo(write_str)
+    	
+    for element in dis_set:
+	logfile.write(str(element) + '\n')
+        rospy.loginfo(str(element))	
+    write_str = "Finished writing record %s" % rospy.get_time()
+    rospy.loginfo(write_str)
     cv2.destroyAllWindows()
     
